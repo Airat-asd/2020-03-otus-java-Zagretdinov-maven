d96a713ca6d251e9f8950ffee6bf56793b226b75
Airat-asd <asd0982@yandex.ru>
2021-02-01 23:08:39 +0500

    HW14

--- HW14-SpingMVC/src/main/resources/migration/V1__initial_schema.sql
+++
@@ -1,9 +0,0 @@
--- Для @GeneratedValue(strategy = GenerationType.IDENTITY)
-
-create table tUsers
-(
-    login varchar(50) not null primary key,
-    name varchar(50) not null,
-    passwordHash integer,
-    isAnAdministrator char(1)
-);
