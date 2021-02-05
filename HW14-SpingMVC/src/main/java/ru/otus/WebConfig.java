dc66a6cd0cfddbef3d4dcaca89d352b5ff5b0008
Айрат <32131437+Airat-asd@users.noreply.github.com>
2021-02-05 17:12:05 +0500

    Update WebConfig.java

--- HW14-SpingMVC/src/main/java/ru/otus/WebConfig.java
+++ HW14-SpingMVC/src/main/java/ru/otus/WebConfig.java
@@ -4,7 +4,6 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.Metadata;
 import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
