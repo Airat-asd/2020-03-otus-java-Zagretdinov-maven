a199e21489654827d1e23ebf4b65a0420b9b6248
Айрат <32131437+Airat-asd@users.noreply.github.com>
2021-02-08 12:21:23 +0500

    Update UserController.java

--- HW14-SpingMVC/src/main/java/ru/otus/controllers/UserController.java
+++ HW14-SpingMVC/src/main/java/ru/otus/controllers/UserController.java
@@ -8,15 +8,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+import ru.otus.businessLayer.dto.DtoUser;
 import ru.otus.businessLayer.model.User;
 import ru.otus.businessLayer.service.DBServiceUser;
+import ru.otus.businessLayer.service.MappingUserToDto;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Controller
 public class UserController {
-    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private final DBServiceUser dbServiceUser;
 
@@ -26,23 +28,22 @@ public class UserController {
 
     @GetMapping({"/", "/user/list"})
     public String clientsListView(Model model) {
-        var test = dbServiceUser.getAllUsers().get();
         List<User> users =  dbServiceUser.getAllUsers().orElse(new ArrayList<>());
-        model.addAttribute("users", users);
+        List<DtoUser> dtoUsers = new ArrayList<>(users.size());
+        users.forEach(user -> dtoUsers.add(MappingUserToDto.mapToDtoUser(user)));
+        model.addAttribute("dtoUsers", dtoUsers);
         return "usersList.html";
     }
 
     @GetMapping("/user/create")
     public String clientCreateView(Model model) {
-        model.addAttribute("user", new User());
+        model.addAttribute("dtoUser", new DtoUser());
         return "userCreate.html";
     }
 
     @PostMapping("/user/save")
-    public RedirectView clientSave(@ModelAttribute User user) {
-        logger.info("!!!!!!!!!!!!!!!!!!!!user = {}", user);
-        dbServiceUser.saveUser(user);
+    public RedirectView clientSave(@ModelAttribute DtoUser dtoUser) {
+        dbServiceUser.saveUser(MappingUserToDto.mapToUser(dtoUser));
         return new RedirectView("/", true);
     }
-
 }
