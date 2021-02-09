242564bea4d65974c6f6031c73ab2da74551c931
Айрат <32131437+Airat-asd@users.noreply.github.com>
2021-02-09 15:58:26 +0500

    Update MappingUserToDto.java

--- HW14-SpingMVC/src/main/java/ru/otus/businessLayer/service/MappingUserToDto.java
+++ HW14-SpingMVC/src/main/java/ru/otus/businessLayer/service/MappingUserToDto.java
@@ -1,59 +1,24 @@
-package ru.otus.businessLayer.dto;
+package ru.otus.businessLayer.service;
 
-public class DtoUser {
-    private String login;
+import org.springframework.stereotype.Service;
+import ru.otus.businessLayer.dto.DtoUser;
+import ru.otus.businessLayer.model.User;
 
-    private String name;
+@Service
+public class MappingUserToDto {
 
-    private String password;
-
-    private char isAnAdministrator = 'n';
-
-    public DtoUser() {
+    public static DtoUser mapToDtoUser(User user) {
+        DtoUser dtoUser = new DtoUser();
+        dtoUser.setName(user.getName());
+        dtoUser.setLogin(user.getLogin());
+        return dtoUser;
     }
 
-    public DtoUser(String login, String name, String password) {
-        this.login = login;
-        this.name = name;
-        this.password = password;
-    }
-
-    public DtoUser(String login, String name, String password, char isAnAdministrator) {
-        this.login = login;
-        this.name = name;
-        this.password = password;
-        this.isAnAdministrator = isAnAdministrator;
-    }
-
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public char getIsAnAdministrator() {
-        return isAnAdministrator;
-    }
-
-    public void setIsAnAdministrator(char isAnAdministrator) {
-        this.isAnAdministrator = isAnAdministrator;
+    public static User mapToUser(DtoUser dtoUser) {
+        User user = new User();
+        user.setName(dtoUser.getName());
+        user.setLogin(dtoUser.getLogin());
+        user.setPasswordHash(dtoUser.getPassword().hashCode());
+        return user;
     }
 }
