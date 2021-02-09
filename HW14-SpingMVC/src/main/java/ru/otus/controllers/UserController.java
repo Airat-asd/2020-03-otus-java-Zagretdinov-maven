142cd01cef7e1c3f0655ca695831253d014def46
Айрат <32131437+Airat-asd@users.noreply.github.com>
2021-02-09 17:50:32 +0500

    Update UserController.java

--- HW14-SpingMVC/src/main/java/ru/otus/controllers/UserController.java
+++ HW14-SpingMVC/src/main/java/ru/otus/controllers/UserController.java
@@ -1,7 +1,5 @@
 package ru.otus.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +12,6 @@ import ru.otus.businessLayer.service.DBServiceUser;
 import ru.otus.businessLayer.service.MappingUserToDto;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Controller
