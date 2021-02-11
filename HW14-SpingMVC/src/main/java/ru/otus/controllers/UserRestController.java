a497bffe9dc737530e6979ffd0a6b7fc105f5690
Айрат <32131437+Airat-asd@users.noreply.github.com>
2021-02-11 15:21:04 +0500

    Update UserRestController.java

--- HW14-SpingMVC/src/main/java/ru/otus/controllers/UserRestController.java
+++ HW14-SpingMVC/src/main/java/ru/otus/controllers/UserRestController.java
@@ -4,8 +4,6 @@ import org.springframework.web.bind.annotation.*;
 import ru.otus.businessLayer.model.User;
 import ru.otus.businessLayer.service.DBServiceUser;
 
-import java.util.Optional;
-
 @RestController
 public class UserRestController {
 
@@ -15,15 +13,10 @@ public class UserRestController {
         this.dbServiceUser = dbServiceUser;
     }
 
-    @GetMapping("/api/client")
-    public User getClientByName(@RequestParam(name = "name") String name) {
-        Optional<User> user = dbServiceUser.getUser(name);
-        return user.orElse(new User());
-    }
-
-    @PostMapping("/api/client")
-    public User saveClient(@RequestBody User user) {
-        dbServiceUser.saveUser(user);
+    @GetMapping("/api/user/{login}")
+    public User getUserByLogin(@PathVariable(name = "login") String login) {
+        User user = dbServiceUser.getUser(login).orElse(new User());
         return user;
     }
+
 }
