538ae7d756173093dbebd010dfaaff18c65d9d9b
Айрат <32131437+Airat-asd@users.noreply.github.com>
2021-03-05 11:01:02 +0500

    Update GRPCClient.java

--- HW17-gRPC/src/main/java/ru/otus/protobuf/client/GRPCClient.java
+++ HW17-gRPC/src/main/java/ru/otus/protobuf/client/GRPCClient.java
@@ -4,7 +4,8 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.otus.protobuf.generated.*;
+import ru.otus.protobuf.generated.RangeNumberRequest;
+import ru.otus.protobuf.generated.RemoteNumberServiceGrpc;
 
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicLong;
@@ -13,23 +14,22 @@ public class GRPCClient {
     private static final Logger logger = LoggerFactory.getLogger(GRPCClient.class);
     private static final String SERVER_HOST = "localhost";
     private static final int SERVER_PORT = 8080;
-    private static final long CYCLE_LIMIT = 20;
+    private static final long CYCLE_LIMIT = 50;
     private static final long FIRST_VALUE = 1;
-    private static final long END_VALUE = 10;
+    private static final long END_VALUE = 30;
     private long value = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         logger.info("Client service is starting...");
         ManagedChannel channel = ManagedChannelBuilder.forAddress(SERVER_HOST, SERVER_PORT)
                 .usePlaintext()
                 .build();
         RemoteNumberServiceGrpc.RemoteNumberServiceStub remoteNumberServiceStub = RemoteNumberServiceGrpc.newStub(channel);
-        new GRPCClient().runClient(remoteNumberServiceStub);
-        logger.info("Client service is over");
-        channel.shutdown();
+        new GRPCClient().runClient(remoteNumberServiceStub, channel);
+
     }
 
-    private void runClient(RemoteNumberServiceGrpc.RemoteNumberServiceStub remoteNumberServiceStub) {
+    private void runClient(RemoteNumberServiceGrpc.RemoteNumberServiceStub remoteNumberServiceStub, ManagedChannel channel) throws InterruptedException {
         RangeNumberRequest request = RangeNumberRequest.newBuilder().setStartNumber(FIRST_VALUE).setEndNumber(END_VALUE).build();
         ClientStreamObserver clientStreamObserver = new ClientStreamObserver();
         remoteNumberServiceStub.getNumber(request, clientStreamObserver);
@@ -43,9 +43,15 @@ public class GRPCClient {
             logger.info("CurrentValue: {}", currentValue);
             if (counter == CYCLE_LIMIT) {
                 executor.shutdown();
+                stopClient(channel);
             }
         };
 
         executor.scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS);
+    }
+
+    private void stopClient(ManagedChannel channel) {
+        logger.info("Client service is over");
+        channel.shutdown();
     }
 }
